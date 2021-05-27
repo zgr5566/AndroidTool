bc39e66a19acd0c223a31824bdbd58d7ab8c88cf
zgr <zgr5566@foxmail.com>
2021-05-27 11:50:50 +0800

    修改

---
+++ app/src/main/java/com/zgr/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zgr.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zgr.tool.list.ListDataUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
